99493a0a39944155262159312da4827df6cfaf00
Newton Rhomel Beck Angelini <newton.beck@gmail.com>
2014-03-01 22:55:21 -0300

    Criou a enumeração de tipos de transferência bancária.

---
+++ src/main/java/br/com/nerdsolutions/transferencia/dominio/modelo/Tipo.java
@@ -0,0 +1,50 @@
+package br.com.nerdsolutions.transferencia.dominio.modelo;
+
+/**
+ * Enumeração de tipos de transferências bancárias.
+ * 
+ * @author Newton Angelini
+ * 
+ */
+public enum Tipo {
+
+	/**
+	 * Operações do tipo A tem uma taxa de $2 mais 3% do valor da trasferência.
+	 */
+	A,
+
+	/**
+	 * Operações do tipo B tem uma taxa de $10 para pedidos com agendamento até
+	 * 30 dias da data de cadastro e $8 para os demais.
+	 */
+	B,
+
+	/**
+	 * Operações do tipo C tem uma taxa regressiva conforme a data de
+	 * agendamento.
+	 * 
+	 * <ul>
+	 * <li>maior que 30 dias da data de cadastro - 1.2%</li>
+	 * <li>até 30 dias da data de cadastro - 2.1%</li>
+	 * <li>até 25 dias da data de cadastro - 4.3%</li>
+	 * <li>até 20 dias da data de cadastro - 5.4%</li>
+	 * <li>até 15 dias da data de cadastro - 6.7%</li>
+	 * <li>até 10 dias da data de cadastro - 7.4%</li>
+	 * <li>até 5 dias da data de cadastro - 8.3%</li>
+	 * </ul>
+	 */
+	C,
+
+	/**
+	 * Operações do tipo D tem a taxa igual a A, B ou C dependendo do valor da
+	 * transferência.
+	 * 
+	 * <ul>
+	 * <li>Valores até $25.000 seguem a taxação tipo A</li>
+	 * <li>Valores de $25.001 até $120000 seguem a taxação tipo B</li>
+	 * <li>Valores maiores que $120000 seguem a taxação tipo C</li>
+	 * </ul>
+	 */
+	D;
+
+}
